de612f434e63812b4f960d9258c410f456f7e291
root <root@LAPTOP-6TC1GQOM.localdomain>
2020-05-25 12:58:35 +0800

    添加主框架。

---
+++ ui/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <component v-bind:is="content"></component>
+        </div>
+        <button @click="onChangeScene">变换</button>
+    </div>
+</template>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+#nav {
+    padding: 30px;
+}
+
+#nav a {
+    font-weight: bold;
+    color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+    color: #42b983;
+}
+</style>
+
+<script>
+import Home from '@/views/Home';
+import About from '@/views/About';
+
+export default {
+    name: 'app',
+    computed: {
+        content() {
+            const scene = this.$store.state.scene;
+            if (scene === 'splash') {
+                return Home;
+            } else {
+                return About;
+            }
+        }
+    },
+    methods: {
+        onChangeScene() {
+            this.$store.commit('changeScene', 'about', null, null);
+        }
+    }
+};
+</script>
---
+++ ui/src/store/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        // 场景
+        scene: 'splash',
+
+        // 阶段
+        stage: null,
+
+        // 步骤
+        step: null
+    },
+    mutations: {
+        /**
+         * 改变场景
+         *
+         * @param {*} state 状态
+         * @param {*} scene 场景
+         * @param {*} stage 阶段
+         * @param {*} step 步骤
+         */
+        changeScene(state, scene, stage, step) {
+            state.scene = scene;
+            state.stage = stage;
+            state.step = step;
+        },
+
+        /**
+         * 改变阶段
+         *
+         * @param {*} state 状态
+         * @param {*} scene 场景
+         * @param {*} stage 阶段
+         */
+        changeStage(state, stage, step) {
+            state.stage = stage;
+            state.step = step;
+        }
+    },
+    actions: {},
+    modules: {}
+});
