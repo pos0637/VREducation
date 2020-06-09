98d91e06dff9b5b910657221f4a0abb9ab422260
root <root@LAPTOP-6TC1GQOM.localdomain>
2020-06-09 15:48:44 +0800

    添加引导说明。

--- ui/src/scenes/RobotStructural.vue
+++ ui/src/scenes/RobotStructural.vue
@@ -6,7 +6,7 @@
                     <span style="font-size: 1.4rem; color: white;">工业机器人内部结构介绍</span>
                 </a-col>
                 <a-col :span="12" style="text-align: right">
-                    <a-button type="primary" icon="forward" style="margin-left: 8px;" @click="_nextUnit">下一单元</a-button>
+                    <a-button data-id="next_unit" type="primary" icon="forward" style="margin-left: 8px;" @click="_nextUnit">下一单元</a-button>
                 </a-col>
             </a-row>
             <a-row style="width: 100%; height: 100%;">
@@ -33,7 +33,40 @@ export default {
     components: {
         Docker
     },
+    data() {
+        return {
+            startPlay: false
+        };
+    },
     mounted() {
+        const options = {
+            nextLabel: '<span style="font-size: 1.0rem">下一步</span>',
+            prevLabel: '<span style="font-size: 1.0rem">上一步</span>',
+            skipLabel: '<span style="font-size: 1.0rem">跳过</span>',
+            doneLabel: '<span style="font-size: 1.0rem; color: blue">开始实验</span>',
+            tooltipPosition: 'auto',
+            exitOnEsc: false,
+            exitOnOverlayClick: false
+        };
+
+        const steps = [
+            {
+                intro: '<iframe frameborder="0" style="width: 500px; height: 400px; magin: 10px;" scrolling="auto" src="guides/structural.html"></iframe>'
+            },
+            {
+                element: document.querySelectorAll('[data-id="next_unit"]')[0],
+                intro: '学习完毕后点击 下一单元 按钮, 进入下一单元学习'
+            }
+        ];
+
+        this.$intro()
+            .setOptions(Object.assign(options, { steps: steps }))
+            .onexit(() => {
+                this.startPlay = true;
+                this._onResize();
+            })
+            .start();
+
         const gameInstance = top.window.gameInstance;
         top.window.onUnityInitialized = () => {
             setTimeout(() => {
@@ -47,6 +80,15 @@ export default {
         top.window.onUnityInitialized = null;
     },
     methods: {
+        _onResize() {
+            if (!this.startPlay) {
+                return;
+            }
+
+            const rect = this.$refs.container.$el.getBoundingClientRect();
+            this.playerOptions.width = rect.width;
+            this.playerOptions.height = rect.height;
+        },
         _nextUnit() {
             this.$store.commit('changeScene', 'robotProgramming', null, null);
         }
